9e066be7f6e7c98b3e18d95a2f0068a1ae8ae6ed
pnilava03 <pnilava03@gmail.com>
2023-08-12 03:20:26 +0530

    Driver class added

---
+++ src/main/java/com/QATestLab/Driver.java
@@ -0,0 +1,26 @@
+package com.QATestLab;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.windows.WindowsDriver;
+
+public class Driver {
+
+	public static WindowsDriver driver = null;
+	
+	public static void startApplication(String applicationPath) throws MalformedURLException {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("app", applicationPath);
+		caps.setCapability("platform", "Windows");
+		caps.setCapability("deviceName", "WindowsPC");
+
+		driver = new WindowsDriver(new URL("http://127.0.0.1:4723/"), caps);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+}
+
--- src/test/java/com/QATestLab/NotePadTest.java
+++ src/test/java/com/QATestLab/NotePadTest.java
@@ -16,37 +16,27 @@ import io.appium.java_client.windows.WindowsDriver;
 
 public class NotePadTest {
 
-	public static WindowsDriver driver = null;
-
 	@SuppressWarnings("deprecation")
 	@BeforeClass
 	public void setUp() throws MalformedURLException {
-		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setCapability("app", "C:\\Windows\\notepad.exe");
-		caps.setCapability("platform", "Windows");
-		caps.setCapability("deviceName", "WindowsPC");
-
-		driver = new WindowsDriver(new URL("http://127.0.0.1:4723/"), caps);
-
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+		Driver.startApplication("C:\\\\Windows\\\\notepad.exe");
 	}
 
 	@AfterMethod
 	public void cleanUp() throws MalformedURLException {
-		driver.quit();
+		Driver.driver.quit();
 		setUp();
 	}
 
 	@AfterSuite
 	public void tearDown() {
-		driver.quit();
+		Driver.driver.quit();
 	}
 
 	@Test(priority = 1)
 	public void open_NewWindowFromFileMenu() {
-		driver.findElement(By.name("File")).click();
-		driver.findElement(By.name("Add New Tab")).click();
+		Driver.driver.findElement(By.name("File")).click();
+		Driver.driver.findElement(By.name("Add New Tab")).click();
 
 	}
 
@@ -54,20 +44,20 @@ public class NotePadTest {
 
 	public void typeOnNotePad() throws InterruptedException {
 		Thread.sleep(5000);
-		driver.findElement(By.name("Maximize")).click();
+		Driver.driver.findElement(By.name("Maximize")).click();
 		Thread.sleep(2000);
 
 //		  driver.findElement(By.className("RichEditD2DPT")).sendKeys("Hello");
 //		  driver.findElement(By.name("File")).click();
 //		  driver.findElement(By.name("Save")).click();
-		
-		driver.findElement(By.name("File")).click();
-		driver.findElement(By.name("Page setup")).click();
-		driver.findElement(By.name("Cancel")).click();
 
-		driver.findElement(By.name("Edit")).click();
-		driver.findElement(By.name("Time/Date")).click();
-	
+		Driver.driver.findElement(By.name("File")).click();
+		Driver.driver.findElement(By.name("Page setup")).click();
+		Driver.driver.findElement(By.name("Cancel")).click();
+
+		Driver.driver.findElement(By.name("Edit")).click();
+		Driver.driver.findElement(By.name("Time/Date")).click();
+
 	}
 
 }
